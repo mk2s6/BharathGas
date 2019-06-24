a39f3d2dd059a650b3b68a7620baf324dbdf1d9c
sivakusi <sivakusi12@gmail.com>
2019-06-24 17:42:34 +0530

    [Dev] added maps

--- public/js/customerDetails.js
+++ public/js/customerDetails.js
@@ -36,6 +36,9 @@ $(function () {
     ";
     // console.log(mapLoc);
     $('#custLocation').html(mapLoc);
+    $('#ui_mobile_call').attr('href', 'tel:+91'+customer.primaryMobile);
+    $('#ui_mobile_msg').attr('href', 'tel:+91'+customer.primaryMobile);
+
   }
 
   $(window).ready(function (e)  {
